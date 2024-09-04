996abee91cde23543404efef30aac16e22208941
siddhartha <siddhartha.pothan@aeris.net>
2024-09-04 16:48:54 +0530

    modified sumExist

--- sumExist.js
+++ sumExist.js
@@ -1,49 +1,8 @@
-// find sum that is equal to target
-
-function hasArrayTwoCandidates(arr, x) {
-    // code here
-    arr = arr.sort((a, b) => a - b)
-    for (let i = 0; i < arr.length; i++) {
-        console.log('itr',i)
-        let left = 0
-        let right = arr.length - 1
-        while (left < right) {
-            if (left == i) {
-                left++;
-                continue
-            }
-
-            if (right == i) {
-                right--
-                continue
-            }
-
-            console.log(left, right)
-            let sum = arr[i] + arr[left]
-            console.log(arr, sum)
-            if (sum === x) {
-                return true
-            } else if(arr[i] + arr[right] === x){
-                return true
-                
-
-            }else {
-                left++;
-            }
-
-        }
-
-    }
-    return false;
-}
+// find two elements sum is == k from a given array
 let x = 16
 let arr = [1, 4, 45, 6, 10, 8]
-//console.log(hasArrayTwoCandidates(arr, x))
-console.log(check(arr,x))
 
 
-/////////////////////////////////////
-// another solution
 function check(arr,x){
     const seen = new Set();
     
@@ -57,5 +16,49 @@ function check(arr,x){
         seen.add(num);
     }
     
+    return false;
+}
+console.log(check(arr,x))
+
+
+
+/////////////////////////////////////////// another solution
+
+function hasPairWithSumTwoPointers(arr, x) {
+    arr.sort((a, b) => a - b);  // Sort the array
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left < right) {
+        const sum = arr[left] + arr[right];
+        if (sum === x) {
+            return true;  // Found a pair
+        } else if (sum < x) {
+            left++;  // Move the left pointer to the right to increase the sum
+        } else {
+            right--; // Move the right pointer to the left to decrease the sum
+        }
+    }
+    return false;  // No pair found
+}
+
+console.log(hasPairWithSumTwoPointers(arr, x))
+
+
+
+/////////////////////////////////////
+
+
+
+
+// brute force approach -  other solution is to loop twice and check the sum
+function hasPairWithSumBruteForce(arr, x) {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] + arr[j] === x) {
+                return true;
+            }
+        }
+    }
     return false;
 }
