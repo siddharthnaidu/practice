cbbcddb29ea69178edb0e976c689c5d67826fab7
siddhartha <siddhartha.pothan@aeris.net>
2024-09-04 17:19:22 +0530

    modified sumpairs

--- sumPairsCount.js
+++ sumPairsCount.js
@@ -1,25 +1,60 @@
 function countPairsWithSum(arr, targetSum) {
     let count = 0;
-    let freqMap = {};
+    let map = {};
 
     for (let num of arr) {
         let complement = targetSum - num;
 
-        // If the complement exists in the map, add the frequency of the complement to the count
-        if (freqMap[complement]) {
-            count += freqMap[complement];
+        // If the complement exists in the map, we will ADD the frequency of the complement to the count because it forms so many pairs
+        if (map[complement]) {
+            console.log('exist', num, map[complement], map)
+            count = count + map[complement];     
         }
 
         // Update the frequency map for the current number
-        freqMap[num] = (freqMap[num] || 0) + 1;
+        map[num] = map[num] ? map[num] + 1 : 1;
+        console.log(map)
     }
-    console.log(freqMap)
 
     return count;
 }
 
-let arr = [1, 2, 3, 4, 5, 5, 5, 6]
+let arr = [0,1, 2, 3, 4, 5, 5, 5, 6,11]
 let k = 11
 const result = countPairsWithSum(arr, k);
 
 console.log(result); // Output: 3
+
+
+//////////////////////////////////////////////
+
+function AllPairsWithSum(arr, targetSum) {
+    let count = 0;
+    let map = {};
+    let pairs = [];
+
+    for (let num of arr) {
+        let complement = targetSum - num;
+
+        // If the complement exists in the map, we will ADD the frequency of the complement to the count because it forms so many pairs
+        if (map[complement]) {
+            console.log('exist', num, map[complement], map)
+            count = count + map[complement];
+
+            // if you need all those pairs
+             for(let i = 0; i<map[complement]; i++){
+                pairs.push([num,complement])
+             }
+
+        }
+
+        // Update the frequency map for the current number
+        map[num] = map[num] ? map[num] + 1 : 1;
+       
+    }
+
+    console.log(count ,"pairs -",pairs)
+    return count;
+}
+
+console.log('All pairs ',AllPairsWithSum(arr,k))
