30074f7646a7902db79ef587974999f1f26bada2
siddhartha <gganeshganesh650@gmail.com>
2024-09-30 18:39:22 +0530

    curry 2nd way solution

--- curry.js
+++ curry.js
@@ -22,3 +22,20 @@ let yes = curriedSum3(1,2)(3)
 let yes2 = curriedSum3(1)(2)(3)
 let yes3 = curriedSum3(1,2,3)
 console.log(yes,' ',yes2,' ',yes3)
+
+
+
+//// another simple one 
+currying = (a) => {
+    return (b) => {
+        if (b) {
+            return currying(a + b)
+        } else {
+            return a
+        }
+     }
+}
+let x = currying(1)(2)(3)(); // note here if its not called in the end it will return function 
+                            //  so call x() like this if needed checkomh typeof(x) == 'function'
+
+console.log(x)
