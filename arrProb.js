a613ded62757223609a5afeb59f561d7477a96cb
siddharthnaidu <43639715+siddharthnaidu@users.noreply.github.com>
2026-04-22 12:19:17 +0530

    Update arrProb.js with duplication array

--- arrProb.js
+++ arrProb.js
@@ -23,4 +23,22 @@ num = num.toString().split('').map((e)=>Number(e)).sort((a,b)=>b-a)
 
 return num[1]
 }
+/////////////////////////////////
+
+let arr = [11, 11, 2, 3, 4, 5, 5];
+
+let dup = [];
+
+let unique = arr.reduce((acc, value) => {
+  if (!acc.includes(value)) {
+    acc.push(value);
+  } else {
+    dup.push(value);
+  }
+  return acc;
+}, []);
+
+console.log("Unique:", unique);
+console.log("Duplicates:", dup);
+``
 
