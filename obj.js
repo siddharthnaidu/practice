0959ee6a762dc88c3548f1a9c111b95f68b841ce
siddharthnaidu <43639715+siddharthnaidu@users.noreply.github.com>
2026-04-23 12:52:15 +0530

    Update obj.js

--- obj.js
+++ obj.js
@@ -9,3 +9,16 @@ obj = null
 console.log(admin) //{ x: 1 }
 
 console.log(obj) // null
+
+// to find which ele repeated how many times
+let arr = [1,2,3,4,4,4,5,6,6];
+
+let c = arr.reduce((a,c)=>{
+if(a[c]){
+    a[c] += 1;
+}else{
+    a[c] = 1;
+}
+return a;
+},{})
+console.log(c)
