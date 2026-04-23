4d55c13111be0327a61d10189161d26a5f3745c5
siddharthnaidu <43639715+siddharthnaidu@users.noreply.github.com>
2026-04-23 12:43:33 +0530

    Update scope.js

--- scope.js
+++ scope.js
@@ -10,8 +10,23 @@ for(var i=0;i<3;i++){
     },0) 
  }
  // here i will be printed 3 (three times ofcourse because its var) because of i++
+
+//if you want to make it print 012 then 
+for(var i=0;i<3;i++){
+   (function(i){
+    setTimeout(()=>{
+     console.log(i);
+
+    },0) 
+   })(i);
+ }
  
- func1();
+
+
+
+
+
+func1();
 
  function func1(){
      console.log('sas')
